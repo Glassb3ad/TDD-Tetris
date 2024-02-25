104f4728552ed7550605a234d1812e0ff44b8c7b
GlassB3ad <j.lahtivirta@gmail.com>
2024-02-25 16:26:15 +0200

    drop implemented

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,18 +1,24 @@
 export class Board {
   width;
   height;
+  falling;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
   }
 
-  drawRow(x = 0, row = "") {
-    return (x <= this.width - 1) ? this.drawRow(++x, row + ".") : row
+  drop(c) {
+    this.falling = { c, x: 1, y: 0 }
+  }
+  drawRow(x = 0, y = 0, row = "") {
+    if (x >= this.width) return row
+    if (this.falling?.x === x && this.falling?.y === y) return this.drawRow(x + 1, y, row + this.falling.c)
+    return this.drawRow(x + 1, y, row + ".")
   }
 
   drawBoard(y = 0, board = "") {
-    return (y <= this.height - 1) ? this.drawBoard(++y, board + `${this.drawRow()}\n`) : board
+    return (y <= this.height - 1) ? this.drawBoard(y + 1, board + `${this.drawRow(0, y)}\n`) : board
   }
 
   toString() {
