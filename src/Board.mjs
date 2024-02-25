f648917ac0017d886049c5f8f23d073be8385e69
GlassB3ad <j.lahtivirta@gmail.com>
2024-02-25 16:46:20 +0200

    refactored y to get smaller as block drops

--- src/Board.mjs
+++ src/Board.mjs
@@ -12,25 +12,25 @@ export class Board {
     if (this.hasFalling()) {
       throw new Error("already falling")
     }
-    this.falling = { c, x: 1, y: 0 }
+    this.falling = { c, x: 1, y: this.height - 1 }
   }
 
   tick() {
-    this.falling.y++
+    this.falling.y--
   }
 
   hasFalling() {
     return !!this.falling
   }
 
-  drawRow(x = 0, y = 0, row = "") {
+  drawRow(x = 0, y = this.height, row = "") {
     if (x >= this.width) return row
     if (this.falling?.x === x && this.falling?.y === y) return this.drawRow(x + 1, y, row + this.falling.c)
     return this.drawRow(x + 1, y, row + ".")
   }
 
-  drawBoard(y = 0, board = "") {
-    return (y <= this.height - 1) ? this.drawBoard(y + 1, board + `${this.drawRow(0, y)}\n`) : board
+  drawBoard(y = this.height - 1, board = "") {
+    return (y >= 0) ? this.drawBoard(y - 1, board + `${this.drawRow(0, y)}\n`) : board
   }
 
   toString() {
