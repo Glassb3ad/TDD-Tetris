697829e2fe15eb070d2e635d5bb7c89e897a5162
GlassB3ad <j.lahtivirta@gmail.com>
2024-03-10 18:13:20 +0200

    I tetrimone added

--- test/RotatingTetrominoes.test.mjs
+++ test/RotatingTetrominoes.test.mjs
@@ -46,7 +46,7 @@ describe("The T shape", () => {
   });
 });
 
-/*
+
 describe("The I shape", () => {
   const shape = Tetromino.I_SHAPE;
 
@@ -84,7 +84,6 @@ describe("The I shape", () => {
     expect(distinctOrientations(shape).size).to.equal(2);
   });
 });
-*/
 
 /*
 describe("The O shape", () => {
