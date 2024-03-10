765ae76a3a3d9ad007442bfeef1e67fe7324e5a5
GlassB3ad <j.lahtivirta@gmail.com>
2024-03-10 18:08:39 +0200

    can be rotated left/counter-clockwise

--- test/RotatingTetrominoes.test.mjs
+++ test/RotatingTetrominoes.test.mjs
@@ -33,7 +33,7 @@ describe("The T shape", () => {
     );
   });
 
-  test.skip("can be rotated left/counter-clockwise", () => {
+  test("can be rotated left/counter-clockwise", () => {
     expect(shape.rotateLeft().toString()).to.equalShape(
       `.T.
        TT.
