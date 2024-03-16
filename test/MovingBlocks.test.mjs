ea70b44912cc154827c468c06ffbe72d4a5e6ed0
GlassB3ad <j.lahtivirta@gmail.com>
2024-03-16 17:54:04 +0200

    created failing test for left movement

---
+++ test/MovingBlocks.test.mjs
@@ -0,0 +1,24 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+
+describe("Falling tetrominoes", () => {
+    let board;
+    beforeEach(() => {
+        board = new Board(10, 6);
+    });
+
+    test("Can be moved left", () => {
+        board.drop(Tetromino.T_SHAPE);
+        board.moveLeft()
+        expect(board.toString()).to.equalShape(
+            `...T......
+         ..TTT.....
+         ..........
+         ..........
+         ..........
+         ..........`
+        );
+    });
+});
