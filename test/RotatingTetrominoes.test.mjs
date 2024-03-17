c44b4d93cbe9ec1fe1e5676d4612908409031002
GlassB3ad <j.lahtivirta@gmail.com>
2024-03-17 10:33:41 +0200

    refacoted test to use Tetrimone2 class

--- test/RotatingTetrominoes.test.mjs
+++ test/RotatingTetrominoes.test.mjs
@@ -1,6 +1,7 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
 import { Tetromino } from "../src/Tetromino.mjs";
+import { Tetromino2 } from "../src/Tetromino.mjs";
 
 function distinctOrientations(shape) {
   const distinct = new Set();
@@ -17,10 +18,11 @@ function distinctOrientations(shape) {
 
 describe("The T shape", () => {
   const shape = Tetromino.T_SHAPE;
+  const shape2 = Tetromino2.T_SHAPE;
   test("initial orientation", () => {
-    expect(shape.toString()).to.equalShape(
-      `.T.
-       TTT
+    expect(shape2.toString()).to.equalShape(
+      `TTT
+       .T.
        ...`
     );
   });
